97ba4ce6c7d5f2c3ea5c709258efa55234774fcf
Justin Gilman <justin@cirle.com>
2015-02-16 15:12:19 -0500

    updating script to replace tags with contents

--- scripts/compile.rb
+++ scripts/compile.rb
@@ -1,15 +1,27 @@
 #!/usr/bin/env ruby
 puts "Work in progress, non-functioning"
 puts "Compiling documents"
-templates = Dir.entries("../templates").select { |f| !File.directory? f}
-documents = Dir.entries("../documents").select { |f| !File.directory? f}
-
+templates = Dir.glob("templates/*").select { |f| !File.directory? f}
+documents = Dir.glob("documents/*").select { |f| !File.directory? f}
 documents.each do |filename|
-	text = File.read("../documents/" + filename)
-	header = File.read("../templates/" + "header.md")
-	newtext = text.gsub(/\$\{header\}/, header);
-	puts newtext;
+	text = ""
+	File.foreach(filename) do |line|
+		newline = line
+		matches = line.scan(/\$\{(.*?)\}/)
+		matches.each do |include|
+			matchstring = include[0].to_s
+			begin
+				included = File.read("templates/#{matchstring}.md")
+				newline = line.gsub(/\$\{#{matchstring}\}/, included);
+			rescue
+				puts "Could not find file \"templates/#{matchstring}.md\""
+			end
+		end
+		text += newline
+	end
+	puts text
+	changelog = `git log --pretty=format:"| %h | %d %s | %cn | %ci |" #{filename}`
 end
 
-gitversion = `git version`
-puts gitversion
+#gitversion = `git version`
+#puts gitversion
